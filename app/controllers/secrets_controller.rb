26e59b790031aebad53bec1ca3ce95ba416d1674
Tyler Sell <tylerselldesign@gmail.com>
2020-04-10 16:52:42 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,4 +1,19 @@
 class SecretsController < ApplicationController
-  def new
+  
+  before_action :require_login
+  
+  def show
+    
   end
+  
+  private 
+  
+  def require_login
+    if !session.include? :name 
+      redirect_to :login 
+    else 
+      render :show 
+    end 
+  end 
+  
 end
